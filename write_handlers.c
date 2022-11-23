49cf4f48ed868a1d34caf7c27c833b7c21272d58
Vito Don Pedro <mrvitodonpedro@gmail.com.com>
2022-11-23 03:12:32 -0800

    ironman

--- write_handlers.c
+++ write_handlers.c
@@ -168,30 +168,29 @@ int handle_precision(va_list args, const char *modifier, char *index)
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 		unsigned char, int, int, unsigned char)
 {
-int i;
-converter_t converters[] = {
-	{'c', convert_c},
-	{'s', convert_s},
-	{'d', convert_di},
-	{'i', convert_di},
-	{'%', convert_percent},
-	{'b', convert_b},
-	{'u', convert_u},
-	{'o', convert_o},
-	{'x', convert_x},
-	{'X', convert_X},
-	{'S', convert_S},
-	{'p', convert_p},
-	{'r', convert_r},
-	{'R', convert_R},
-	{0, NULL}
-};
+	int i;
 
-for (i = 0; converters[i].func; i++)
-{
-if (converters[i].specifier == *specifier)
-return (converters[i].func);
-}
-
-return (NULL);
+	converter_t converters[] = {
+		{'c', convert_c},
+		{'s', convert_s},
+		{'d', convert_di},
+		{'i', convert_di},
+		{'%', convert_percent},
+		{'b', convert_b},
+		{'u', convert_u},
+		{'o', convert_o},
+		{'x', convert_x},
+		{'X', convert_X},
+		{'S', convert_S},
+		{'p', convert_p},
+		{'r', convert_r},
+		{'R', convert_R},
+		{0, NULL}
+	};
+	for (i = 0; converters[i].func; i++)
+	{
+		if (converters[i].specifier == *specifier)
+			return (converters[i].func);
+	}
+	return (NULL);
 }
